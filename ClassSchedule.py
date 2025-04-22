34339ff001507752be47425995630ae650101bf5
890s <147666213+890s@users.noreply.github.com>
2025-04-22 11:43:22 -0400

    Update ClassSchedule.py

---
+++ ClassSchedule.py
@@ -0,0 +1,9 @@
+class ClassSchedule:
+  def __init__(self, mat, sci, engl, ss, elec1, elec2):
+    self.math = mat
+    self.science = sci
+    self.english = engl
+    self.ss = ss
+    self.elect1 = elec1
+    self.elect2 = elec2
+
