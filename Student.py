d2b817d18be58d54b2afb8d1f705ed9c07ddbefa
890s <147666213+890s@users.noreply.github.com>
2025-04-22 11:37:28 -0400

    Update Student.py

---
+++ Student.py
@@ -0,0 +1,7 @@
+class Student:
+  def __init__(self, first, last, grade, courses, selection):
+    self.nameFirst = first
+    self.nameLast = last
+    self.gradeLevel = grade
+    self.coursesTaken = courses
+    self.coursesSel = selection
