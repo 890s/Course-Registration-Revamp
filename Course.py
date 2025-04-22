054d86d7b0f4c0c891a99a7b747f448ee3178c2f
890s <147666213+890s@users.noreply.github.com>
2025-04-22 11:46:24 -0400

    Update Course.py

--- Course.py
+++ Course.py
@@ -1,5 +1,6 @@
 class Course:
-  def __init__(self, prereq="", gradeReq=[9,10,11,12], credit=[]):
+  def __init__(self, prereq="", gradeReq=[9,10,11,12], credit=[], teacher):
     self.prereqs = prereq
     self.gradeReqs = gradeReq
     self.credits = credit
+    self.teacher = teacher
