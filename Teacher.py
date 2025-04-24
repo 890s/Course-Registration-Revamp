931bea8e44b37adc074de18d8f131799f0ac588d
890s <147666213+890s@users.noreply.github.com>
2025-04-24 11:37:38 -0400

    Update Teacher.py

--- Teacher.py
+++ Teacher.py
@@ -1,6 +1,9 @@
 class Teacher:
   def __init(self, first, last, subject, students)__:
-    self.nameFirst = first
-    self.nameLast = last
-    self.subject = subject
-    self.students = students
+    self.nameFirst = first #string
+    self.nameLast = last #string
+    self.subject = subject # string
+    self.students = students #double array: index 1 is period, index 2 is student in period
+
+  def addStudent(self, student, period):
+    self.students[period-1].append(student)
