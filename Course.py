1724820c484b996846b15e0f001a75210fa1dc61
890s <147666213+890s@users.noreply.github.com>
2025-05-19 21:06:32 -0400

    Update Course.py

--- Course.py
+++ Course.py
@@ -1,6 +1,11 @@
 class Course:
-  def __init__(self, prereq="", gradeReq=[9,10,11,12], credit=[], teacher):
-    self.prereqs = prereq
-    self.gradeReqs = gradeReq
-    self.credits = credit
-    self.teacher = teacher
+    def __init__(self, name, internal, id, subject, prereqs=[], gradereq=[9,10,11,12]):
+        self.name = name
+        self.internal = internal
+        self.id = id
+        self.subject = subject
+        self.prereqs = prereqs
+        self.gradereq = gradereq
+    
+    def __str__(self):
+        return self.name
